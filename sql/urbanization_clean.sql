6a619b23c26ed2220a9d29afbb2dd05321d9e5f5
Callum Hughes <callumhughes38@gmail.com>
2025-05-20 15:41:50 +0100

    Delete sql/urbanization_clean.sql

--- sql/urbanization_clean.sql
+++
@@ -1,9 +0,0 @@
--- Create a cleaned urbanization table with only needed columns
-CREATE TABLE urbanization_clean AS
-SELECT 
-    "Entity" AS country,
-    "Year" AS year,
-    -- Choose one urban population percentage column (using the one with projections)
-    "Urban population (%) long-run with 2050 projections (OWID)" AS urban_population_pct,
-    "Urban population long-run with 2050 projections (OWID)" AS urban_population_total
-FROM Urbanization_Data;
